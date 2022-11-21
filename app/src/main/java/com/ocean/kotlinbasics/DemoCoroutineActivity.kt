43a728e7dfd492b70f06e0013862339a56ba8c2a
PRAGYANSHREE DAS <pragyan9423@gmail.com>
2022-11-21 23:57:20 +0530

    initial commit

---
+++ app/src/main/java/com/ocean/kotlinbasics/DemoCoroutineActivity.kt
@@ -0,0 +1,11 @@
+package com.ocean.kotlinbasics
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DemoCoroutineActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_demo_coroutine)
+    }
+}
