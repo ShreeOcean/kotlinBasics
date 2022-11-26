55d2b3e89ce994ef6afbae0766f8e58e3e4b94d8
Pragyanshree Das <shree.pragyan23@gmail.com>
2022-11-26 17:00:38 +0530

    dice roll code in kotlin completed with new version

--- app/src/main/java/com/ocean/kotlinbasics/DiceRollActivity.kt
+++ app/src/main/java/com/ocean/kotlinbasics/DiceRollActivity.kt
@@ -26,7 +26,17 @@ class DiceRollActivity : AppCompatActivity() {
 
     private fun rollDice() {
         val dice = Dice(6)
-        val diceRoll = dice.roll()
-        binding.tvDiceRoll.text = diceRoll.toString()
+        val num = 6
+        val luckyNumber = (1..num).random()
+
+        when (dice.roll()) {
+            luckyNumber -> binding.tvDiceRoll.text = dice.roll().toString() + (" You won! your lucky roll is $luckyNumber" )
+            1 -> binding.tvDiceRoll.text = dice.roll().toString() + (" So sorry! You rolled a 1. Try again for $luckyNumber !")
+            2 -> binding.tvDiceRoll.text = dice.roll().toString() + (" Sadly, you rolled a 2. Try again for $luckyNumber !")
+            3 -> binding.tvDiceRoll.text = dice.roll().toString() + (" Unfortunately, you rolled a 3. Try again for $luckyNumber !")
+            5 -> binding.tvDiceRoll.text = dice.roll().toString() + (" Don't cry! You rolled a 5. Try again for $luckyNumber !")
+            6 -> binding.tvDiceRoll.text = dice.roll().toString() + (" Apologies! You rolled a 6. Try again for $luckyNumber !")
+        }
+
     }
 }
