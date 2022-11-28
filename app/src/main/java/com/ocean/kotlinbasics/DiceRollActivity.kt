6140e31d6d615067111448865448427826a670c9
Pragyanshree Das <shree.pragyan23@gmail.com>
2022-11-28 11:52:37 +0530

    not satisfied rethink of logic is needed

--- app/src/main/java/com/ocean/kotlinbasics/DiceRollActivity.kt
+++ app/src/main/java/com/ocean/kotlinbasics/DiceRollActivity.kt
@@ -15,6 +15,7 @@ class DiceRollActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnDiceRoll.setOnClickListener { rollDice() }
+        rollDice()
     }
 
     class Dice(val numSides: Int) {
@@ -29,34 +30,45 @@ class DiceRollActivity : AppCompatActivity() {
         val num = 6
         val luckyNumber = (1..num).random()
 
-        when (dice.roll()) {
-            luckyNumber -> binding.tvDiceRoll.text = " You won! your lucky roll is $luckyNumber"
-//            luckyNumber -> binding.imageViewDice.setImageDrawable()
-            1 -> {
-                binding.tvDiceRoll.text = " So sorry! You rolled a 1. Try again for $luckyNumber !"
-                binding.imageViewDice.setImageResource(R.drawable.dice_1)
-            }
-            2 -> {
-                binding.tvDiceRoll.text = " Sadly, you rolled a 2. Try again for $luckyNumber !"
-                binding.imageViewDice.setImageResource(R.drawable.dice_2)
-            }
-            3 -> {
-                binding.tvDiceRoll.text = " Unfortunately, you rolled a 3. Try again for $luckyNumber !"
-                binding.imageViewDice.setImageResource(R.drawable.dice_3)
-            }
-            4 -> {
-                binding.tvDiceRoll.text = " Unfortunately, you rolled a 4. Try again for $luckyNumber !"
-                binding.imageViewDice.setImageResource(R.drawable.dice_4)
-            }
-            5 -> {
-                binding.tvDiceRoll.text = " Don't cry! You rolled a 5. Try again for $luckyNumber !"
-                binding.imageViewDice.setImageResource(R.drawable.dice_5)
-            }
-            6 -> {
-                binding.tvDiceRoll.text = " Apologies! You rolled a 6. Try again for $luckyNumber !"
-                binding.imageViewDice.setImageResource(R.drawable.dice_6)
-            }
+        val drawableResource = when (dice.roll()){
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
+        binding.imageViewDice.setImageResource(drawableResource)
+        binding.imageViewDice.contentDescription = dice.roll().toString()
+
+//        when (dice.roll()) {
+////            luckyNumber -> binding.tvDiceRoll.text = " You won! your lucky roll is $luckyNumber"
+////            luckyNumber -> binding.imageViewDice.setImageDrawable()
+//            1 -> {
+////                binding.tvDiceRoll.text = " So sorry! You rolled a 1. Try again for $luckyNumber !"
+//                binding.imageViewDice.setImageResource(R.drawable.dice_1)
+//            }
+//            2 -> {
+////                binding.tvDiceRoll.text = " Sadly, you rolled a 2. Try again for $luckyNumber !"
+//                binding.imageViewDice.setImageResource(R.drawable.dice_2)
+//            }
+//            3 -> {
+////                binding.tvDiceRoll.text = " Unfortunately, you rolled a 3. Try again for $luckyNumber !"
+//                binding.imageViewDice.setImageResource(R.drawable.dice_3)
+//            }
+//            4 -> {
+////                binding.tvDiceRoll.text = " Unfortunately, you rolled a 4. Try again for $luckyNumber !"
+//                binding.imageViewDice.setImageResource(R.drawable.dice_4)
+//            }
+//            5 -> {
+////                binding.tvDiceRoll.text = " Don't cry! You rolled a 5. Try again for $luckyNumber !"
+//                binding.imageViewDice.setImageResource(R.drawable.dice_5)
+//            }
+//            6 -> {
+////                binding.tvDiceRoll.text = " Apologies! You rolled a 6. Try again for $luckyNumber !"
+//                binding.imageViewDice.setImageResource(R.drawable.dice_6)
+//            }
+//        }
 
     }
 }
